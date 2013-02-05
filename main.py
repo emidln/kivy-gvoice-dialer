a462b33252965b2d281ef71b5a05fe6b0aabbff3
Brandon Adams <emidln@gmail.com>
2013-02-04 20:31:11 -0500

    hijKLmnop

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class DialPadButton(Button):
 
 dial_pad_data = {
     '1': '    ', '2': ' abc', '3': ' def',
-    '4': ' ghi', '5': ' jlk', '6': ' mno',
+    '4': ' ghi', '5': ' jkl', '6': ' mno',
     '7': 'pqrs', '8': ' tuv', '9': 'wxyz',
     '*': '    ', '0': '    ', '#': '    ',
 }
